bf8dbd75fb7a7f851a06be185da2ab5bd5f22cb8
Josh Perez <josh@goatslacker.com>
2015-10-15 19:47:46 -0700

    Shitty validation

--- src/fluxForm.js
+++ src/fluxForm.js
@@ -13,6 +13,7 @@ export const getActionCreators = (namespace) => {
   const saveId = `${namespace}/saved`
   const cancelId = `${namespace}/canceled`
   const changeId = `${namespace}/changed`
+  const failId = `${namespace}/failed`
 
   return {
     saved: {
@@ -26,7 +27,11 @@ export const getActionCreators = (namespace) => {
     changed: {
       id: changeId,
       dispatch: data => fsa(changeId, data),
-    }
+    },
+    failed: {
+      id: failId,
+      dispatch: data => fsa(failId, data),
+    },
   }
 }
 
@@ -34,20 +39,39 @@ export default (namespace, dispatcher, opts) => {
   const { fields, state, validators } = opts
 
   // get the action creators
-  const { changed, saved, canceled } = getActionCreators(namespace)
+  const { changed, saved, canceled, failed } = getActionCreators(namespace)
 
   // build action dispatchers
-  const save = () => dispatcher.dispatch(saved.dispatch(state))
+  const save = (callback) => {
+    const invalidState = Object.keys(validators).reduce((x, key) => {
+      try {
+        validators[key](state[key])
+      } catch (e) {
+        x[key] = e.message
+      }
+      return x
+    }, {})
+
+    const isValid = !Object.keys(invalidState).length
+
+    if (isValid) {
+      dispatcher.dispatch(saved.dispatch(state));
+    } else {
+      dispatcher.dispatch(failed.dispatch(invalidState));
+    }
+
+    if (callback) callback(isValid, state)
+  }
   const cancel = () => dispatcher.dispatch(canceled.dispatch(state))
-  const change = () => dispatcher.dispatch(changed.dispatch(state))
+  const change = (key, val) => {
+    state[key] = val
+    dispatcher.dispatch(changed.dispatch(state))
+  }
 
   // the onChange handler for fields
   const onChange = (ev) => {
     const target = ev.target
-    if (target.dataset.fluxKey) {
-      state[target.dataset.fluxKey] = target.value
-      change()
-    }
+    if (target.dataset.fluxKey) change(target.dataset.fluxKey, target.value)
   }
 
   // the fields as props
@@ -61,6 +85,6 @@ export default (namespace, dispatcher, opts) => {
   }, {})
 
   return {
-    form: { props, save, cancel, state }
+    form: { props, save, cancel, change, state }
   }
 }
