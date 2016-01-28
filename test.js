29928687a0ba750543f1664e952b8b33523a3b7a
Josh Perez <josh@goatslacker.com>
2016-01-27 18:26:49 -0800

    tests

--- test.js
+++ test.js
@@ -13,30 +13,92 @@ const propTypes = {
   age: PropTypes.number,
 }
 
-const success = (msg, test) => x => ok(test(x), msg)
+const test = f => x => f(x)
 const fail = msg => () => ok(false, msg)
 
-const makeForm = (state) => {
-  return fluxForm('Person', { dispatch() { } }, { propTypes, state })
+const spec = (name, state, onSuccess, onFailure) => {
+  console.log(`> ${name}...`)
+  return fluxForm(
+    name,
+    { dispatch() { } },
+    { propTypes, state }
+  ).validate()
+  .then(onSuccess, onFailure)
+  .then(() => {
+    console.log(`√ ${name}`)
+  })
+  .catch((err) => {
+    console.error(`ø ${name}`)
+    console.error(err.stack)
+    return Promise.reject(err)
+  })
 }
 
-const form = makeForm({
-  name: 'Josh',
-  age: 'fuck',
-})
+const runner = (specs) => {
+  return Promise.all(specs).then(() => console.log('OK.'), () => {
+    console.log('Failed.')
+    process.exit(1);
+  })
+}
 
-form.validate().then(state => console.log('ok', state), (state) => console.log(state))
-
-//makeForm({
-//  name: 'Josh',
-//  age: 'fuck',
-//}).validate()
-//.then((state) => { console.log('@', state)})
-//.then(fail('Form did not validate properly'))
-//.catch(success('Error was thrown', err => {
-//  console.log(err)
-//  return false
-//}))
-
-// XXX
-// OK now that we're using propTypes we'll want to make sure we do something...
+runner([
+  spec(
+    'Data missing',
+    {},
+    fail('Form validated properly'),
+    test((state) => {
+      ok(state.name instanceof Error, 'state.name is missing')
+      ok(state.stats instanceof Error, 'state.stats is missing')
+    })
+  ),
+  spec(
+    'Name is there',
+    {
+      name: 'Josh',
+      age: 14,
+    },
+    fail('Form validated properly'),
+    test((state) => {
+      ok(state.name === 'Josh', 'state.name is present')
+    })
+  ),
+  spec(
+    'Stats is empty',
+    {
+      name: 'Josh',
+      stats: {},
+      age: 21,
+    },
+    fail('Form validated properly'),
+    test((state) => {
+      ok(state.stats instanceof Error, 'state.stats is an empty obj')
+    })
+  ),
+  spec(
+    'Stats has incorrect props',
+    {
+      name: 'Josh',
+      stats: { height: 151, weight: '83' },
+      age: 28,
+    },
+    fail('Form validated properly'),
+    test((state) => {
+      ok(state.stats instanceof Error, 'state.stats is an empty obj')
+    })
+  ),
+  spec(
+    'All good',
+    {
+      name: 'Josh',
+      stats: { height: 149, weight: 101 },
+      age: 35,
+    },
+    test((state) => {
+      ok(state.name === 'Josh')
+      ok(state.stats.height === 149)
+      ok(state.stats.weight === 101)
+      ok(state.age === 35)
+    }),
+    fail('Form had an error')
+  ),
+])
